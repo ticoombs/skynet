43b34dee34ec4e13607e4b858330fd4563070630
ticoombs <timc@slowb.ro>
2015-12-21 15:56:54 +1100

    Stage 1: Framework

---
+++ ai.py
@@ -0,0 +1,8 @@
+import __future__
+import skynet
+import timetravel
+import sys
+
+major, minor, micro, releaselevel, serial = sys.version_info
+print("Skynet AI is not yet python %s.%s compatible " % (major, minor))
+sys.exit(1)
